9d4fd76aff577c016427d25c5b0888d73b0d4c09
Cooper Mayne <coopermayne@gmail.com>
2015-04-10 15:40:01 -0400

    completely refactored-- working nice

---
+++ game.js
@@ -0,0 +1,301 @@
+app.factory('Game', ['$timeout', '$rootScope', function(timeout, rootScope) {
+
+  var Game = function(config) {
+    //instantiate a new game
+    console.log('game instantiated');
+
+    this.current_lvl = 0
+
+
+    //make a new game object
+    this.game = new WGo.Game(19);
+
+    //draw the board
+    this.board = new WGo.Board(config.element, {
+      width: 600,
+      stoneHandler: WGo.Board.drawHandlers.MONO,
+      size: this.game.size,
+      background: "",
+      section: {
+        top: 4, right: 4, bottom: 4, left: 4
+      },
+    });
+
+    //store current listener here...
+    this.board.listener = null;
+
+    this.levels = [
+      {
+        id:0,
+        type: 'static',
+        description: 'capture one stone',
+        init_moves: [
+          {x: 9, y: 9, color: WGo.B}
+        ]
+      },
+      {
+        id:1,
+        type: 'static',
+        description: 'capture two stones',
+        init_moves: [
+          {x: 9, y: 10, color: WGo.B},
+          {x: 9, y: 9, color: WGo.B}
+        ]
+      },
+      {
+        id:2,
+        type: 'static',
+        description: 'capture three stones',
+        init_moves: [
+          {x: 9, y: 10, color: WGo.B},
+          {x: 9, y: 9, color: WGo.B},
+          {x: 10, y: 9, color: WGo.B}
+        ]
+      },
+      {
+        id:3,
+        type: 'dynamic',
+        ai_group: {x: 8, y:11},
+        description: 'capture using ladder',
+        init_moves: [
+          {x: 7, y: 10, color: WGo.W},
+          {x: 7, y: 11, color: WGo.W},
+          {x: 8, y:12, color: WGo.W},
+          {x: 8, y: 11, color: WGo.B},
+          {x: 12, y:7, color: WGo.W},
+        ]
+      },
+      {
+        id:4,
+        type: 'dynamic',
+        ai_group: {x: 8, y:11},
+        description: 'capture using net',
+        init_moves: [
+          {x: 7, y: 10, color: WGo.W},
+          {x: 7, y: 11, color: WGo.W},
+          {x: 8, y:12, color: WGo.W},
+          {x: 9, y:12, color: WGo.W},
+          {x: 8, y: 11, color: WGo.B},
+          {x: 12, y: 7, color: WGo.B},
+        ]
+      },
+      {
+        id:5,
+        type: 'static',
+        description: 'capture group with one eye',
+        init_moves: [
+          {x: 9, y: 10, color: WGo.B},
+          {x: 8, y: 10, color: WGo.B},
+          {x: 10, y: 10, color: WGo.B},
+          {x: 9, y: 8, color: WGo.B},
+          {x: 8, y: 8, color: WGo.B},
+          {x: 10, y: 8, color: WGo.B},
+          {x: 8, y: 9, color: WGo.B},
+          {x: 10, y: 9, color: WGo.B},
+        ]
+      },
+      {
+        id:6,
+        type: 'dynamic',
+        description: 'protect group by making two eyes',
+        init_moves: [
+        ]
+      }
+    ];
+
+  }
+
+  Game.prototype.helpers = {
+
+    idxToCoord: function(index, size) {
+      var x = Math.floor(index/size);
+      var y = index%size;
+      return {
+        x: x,
+        y: y
+      }
+    }
+  }
+
+
+  Game.prototype.setUpPosition = function() {
+    console.log('set up position');
+    this.board.removeAllObjects();
+    var schema = this.game.position.schema
+
+    for (var i = 0, len = schema.length; i < len; i++) {
+      if (schema[i] == 1) {
+        this.board.addObject({
+          x: this.helpers.idxToCoord(i, this.board.size).x,
+          y: this.helpers.idxToCoord(i, this.board.size).y,
+          c: WGo.B
+        })
+      } else if (schema[i]==-1){
+        this.board.addObject({
+          x: this.helpers.idxToCoord(i, this.board.size).x,
+          y: this.helpers.idxToCoord(i, this.board.size).y,
+          c: WGo.W
+        })
+      } else {
+
+      }
+    }
+  }
+
+  Game.prototype.getCurrentLevel = function() {
+    //returns the level object with all info for current lvl
+    return this.levels[this.current_lvl]
+  }
+
+  Game.prototype.clearListener = function() {
+    //clear off listener
+    this.board.removeEventListener("click", this.board.listener);
+  }
+
+  Game.prototype.setUpListener = function() {
+    var lvl = this.getCurrentLevel();
+    var self = this
+
+    this.board.listener = function(x,y) {
+      //for one player problems
+      self.staticListener(x,y);
+    }
+
+    this.board.addEventListener('click', this.board.listener);
+  }
+
+  Game.prototype.staticListener = function(x,y) {
+    console.log('listener');
+    var board = this.board;
+    var game = this.game;
+
+    //handle invalid moves
+    if (!game.isValid(x,y, WGo.W)) {return false}
+    //remove any captured stones from the board
+    var captures = game.play(x,y, WGo.W);
+    angular.forEach(captures, function(value) {
+      board.removeObject(value);
+    });
+
+    //add the valid move
+    board.addObject({
+      x: x,
+      y: y,
+      c: WGo.W
+    });
+
+    //check if you won
+    if (game.getCaptureCount(WGo.W) > 0) {
+      //no more playing!
+      board.removeEventListener("click", board.listener);
+      //go to next lvl
+      this.current_lvl += 1;
+
+      //inform the scope
+      console.log('BROADCAST: game won');
+      rootScope.$broadcast('win');
+      return;
+    }
+    
+    if (this.getCurrentLevel().type == 'dynamic') {
+      //if its a problem that needs and answer...
+      //prevent further moves and tell scope whats happening 
+
+      //store for reattachment
+      board._listener = board.listener;
+      board.removeEventListener("click", board.listener);
+
+      console.log('BROADCAST: AI turn');
+      rootScope.$broadcast('ai_turn');
+      return;
+    }
+  }
+
+  Game.prototype.aiRespond = function() {
+    //make a smart move and put event listener back on
+    console.log('responding');
+
+    //recursive function -- find group and liberties-- 
+    var self = this
+    var examine_group = function(tested, liberties, x, y, color) {
+      if (self.game.position.get(x,y) == 0) {
+        //if no stone there add to group liberties and return
+        liberties.push({x: x, y: y});
+        return
+      }
+      if (self.game.position.get(x,y) == -color) {
+        //if opposite color just end the recurse
+        return
+      }
+      //if already tested -- end recursion
+      if (tested.get(x,y) !== 0) {
+        return
+      }
+
+      //add me to group
+      tested.set(x,y,true);
+
+      //check neighbors
+      //up
+      examine_group(tested, liberties, x, y-1, color)
+      //right
+      examine_group(tested, liberties, x+1, y, color)
+      //down
+      examine_group(tested, liberties, x, y+1, color)
+      //left
+      examine_group(tested, liberties, x-1, y, color)
+    }
+
+    var testSchema = new WGo.Position(this.game.size)
+    var libs = []
+    var black_group = {x:8, y:11, color: 1}
+    examine_group(testSchema, libs, black_group.x, black_group.y, black_group.color)
+
+    //play for maximum liberty extension
+    var lx = libs[0].x
+    var ly = libs[0].y
+    this.game.play(lx, ly, WGo.B)
+    this.board.addObject({
+      x: lx,
+      y: ly,
+      c: WGo.B
+    });
+
+    //reattach listener
+    this.board.addEventListener('click', this.board._listener);
+  }
+
+  
+  Game.prototype.setUpProblem = function(scope) {
+    console.log('setting up problem');
+    var game = this.game;
+    var board = this.board;
+
+    //reset game and listeners
+    game.firstPosition();
+    this.clearListener();
+
+    //get array of initial moves for problem
+    var init_moves = this.getCurrentLevel().init_moves;
+
+    angular.forEach(init_moves, function(stone) {
+      //sync game position with pattern for problem
+      game.play(stone.x,stone.y,stone.color)
+    })
+    //populate board with pattern for problem
+    this.setUpPosition();
+
+    //set up event handlers
+    this.setUpListener();
+  }
+
+  Game.prototype.restart = function() {
+    console.log('restart');
+    var self = this;
+    timeout( function() {
+      self.setUpProblem();
+    },500)
+  }
+
+  return Game;
+}]);
